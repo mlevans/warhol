2fc26e0c7a05bb581bf0e7645a2ae74123d137a6
Michael Lawrence Evans <michael@codeforamerica.org>
2013-11-17 00:06:15 -0500

    Taking care of initial image that gets created at the start of the gPhoto process

--- utilities/initialize_gphoto.sh
+++ utilities/initialize_gphoto.sh
@@ -1,1 +1,9 @@
+# Run from root directory
+INITIAL_PICTURE=static/pictures/1.JPG
+
+if [ -f $INITIAL_PICTURE ];
+then
+	rm $INITIAL_PICTURE
+fi
+
 gphoto2 --capture-image-and-download --filename "static/pictures/%n.JPG" -F 100000 -I -1
