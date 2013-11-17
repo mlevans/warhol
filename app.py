5bf4b158a9f1a03ba2e3849caba50b9fe6e84660
Michael Lawrence Evans <michael@codeforamerica.org>
2013-11-17 18:12:57 -0500

    Tracking stdout and stderr with subprocess and fixing some bugs

--- app.py
+++ app.py
@@ -41,6 +41,9 @@ def home():
 
 @app.route('/_take_pictures')
 def take_pictures():
+    ###
+    # TODO: Find previous pictures if any and delete them
+    ###
     if app.config['USE_GPHOTO']:
         # FIRST METHOD
         # For capture with 6 second delay
@@ -69,17 +72,26 @@ def take_pictures():
         
         for i in range(app.config['NUMBER_OF_PHOTOS']):
             #subprocess.call('kill -USR1 ' + GPHOTO2_PID, shell=True)
-            
-            time.sleep(app.config['INTERVAL_TIME'])
+            gphoto_proc = subprocess.Popen(["kill", "-USR1", GPHOTO2_PID],stdout=subprocess.PIPE)
 
+            stdoutdata, stderrdata = gphoto_proc.communicate()
+
+            print 'stdout: ',stdoutdata, 'stderr: ', stderrdata
+
+            time.sleep(app.config['INTERVAL_TIME'])
     elif app.config['USE_EOSUTILITY']:
         # Add a slight delay so that people can adjust themselves for the photos.
         time.sleep(2)
-        subprocess.call('open photobooth.app/', shell=True)
+        
+        # Open the photobooth app
+        # subprocess.call('open photobooth.app/', shell=True)
+        # subprocess.call(["open","photobooth.app/"])
+        photobooth_proc = subprocess.Popen(["open", "photobooth.app"],stdout=subprocess.PIPE)
+        stdoutdata, stderrdata = photobooth_proc.communicate()
+        print 'stdout: ',stdoutdata, 'stderr: ', stderrdata
+
         # Wait for the photos to finish
         time.sleep(app.config['SHOOT_DURATION'])
-    
-        #subprocess.call(["open","photobooth.app/"])
     else:
         raise Exception("You need to specify a program to automate your camera.")
     
@@ -96,10 +108,10 @@ def take_pictures():
     # Get the latest pictures
     picture_list = glob.glob("static/pictures/*.JPG")
 
-    if picture_list == 0:
+    if picture_list:
         # Move the photos
         for i, picture_path in enumerate(picture_list):
-            if EOSUTILITY or picture_path != "static/pictures/1.JPG":
+            if (app.config['USE_EOSUTILITY'] and not app.config['USE_GPHOTO']) or picture_path != "static/pictures/1.JPG":
                 shutil.move(picture_list[i], "static/pictures/sets/" + timestamp)
     else:
         raise Exception("No photos with that naming scheme were found!")
